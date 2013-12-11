2a0e433ed02250eb004d547fdec99482c6fad01e
Scott M. Likens <scott@likens.us>
2013-12-10 19:22:49 -0800

    it is now 0.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'scott@likens.us'
 license          'Apache 2.0'
 description      'Installs/Configures beaver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 depends "git"
 depends "build-essential"
